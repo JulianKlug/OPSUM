a25a5c8880a2bb7196f8516466942de3c17fec5f
Julian Klug <tensu.wave@gmail.com>
2022-08-09 14:09:08 +0200

    Ensure EDS end digits have 4 digits for case admission id

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -13,7 +13,7 @@ def remove_french_accents_and_cedillas_from_dataframe(df: pd.DataFrame) -> pd.Da
 def create_ehr_case_identification_column(df):
     # Identify each case with case id (patient id + eds last 4 digits)
     case_identification_column = df['patient_id'].astype(str) \
-                                 + '_' + df['eds_end_4digit'].astype(str)
+                                 + '_' + df['eds_end_4digit'].str.zfill(4).astype(str)
     return case_identification_column
 
 
@@ -25,6 +25,6 @@ def create_registry_case_identification_column(df):
     if 'EDS_last_4_digits' not in df.columns:
         df['EDS_last_4_digits'] = df['Case ID'].apply(lambda x: x[-4:]).astype(str)
     case_identification_column = df['patient_id'].astype(str) \
-                                 + '_' + df['EDS_last_4_digits'].astype(str)
+                                 + '_' + df['EDS_last_4_digits'].str.zfill(4).astype(str)
     return case_identification_column
 
