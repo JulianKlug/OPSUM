9d2d81a9983a23d5f37aec8d47529c72c68cb7c0
Julian Klug <tensu.wave@gmail.com>
2022-08-18 12:22:22 +0200

    Updated scales preprocessing - automated detection of overwritten patient_ids - automated correction of overwritten patient_ids - ensure scales are numerical

--- preprocessing/scales_preprocessing/scales_preprocessing.py
+++ preprocessing/scales_preprocessing/scales_preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 
-from preprocessing.utils import create_ehr_case_identification_column
+from preprocessing.utils import create_ehr_case_identification_column, correct_overwritten_patient_id
 
 
 def restrict_variable_to_possible_ranges(df, variable_name, possible_value_ranges, verbose=False):
@@ -18,7 +18,7 @@ def restrict_variable_to_possible_ranges(df, variable_name, possible_value_range
     if verbose:
         print(f'Excluding {clean_df.score.isna().sum()} observations because out of range')
     excluded_df = df[clean_df.score.isna()]
-    clean_df = clean_df.dropna()
+    clean_df = clean_df.dropna(subset=['score'])
     return clean_df, excluded_df
 
 def preprocess_scales(scales_df, eds_df, verbose=False):
@@ -31,37 +31,46 @@ def preprocess_scales(scales_df, eds_df, verbose=False):
     :return:
     """
 
-    scales_df['original_patient_id'] = scales_df['patient_id'].apply(lambda x: eds_df[eds_df['eds_final_patient_id'] == x]['patient_id'].iloc[0])
+    # Correct for overwritten patient_id
+    scales_df = correct_overwritten_patient_id(scales_df, eds_df)
 
     scales_df['case_admission_id'] = create_ehr_case_identification_column(scales_df)
 
-    columns_to_drop = ['nr', 'patient_id', 'eds_end_4digit', 'eds_manual', 'DOB', 'begin_date',
+    columns_to_drop = ['patient_id', 'eds_end_4digit', 'eds_manual', 'DOB', 'begin_date',
                        'end_date', 'death_date', 'death_hosp', 'eds_final_id',
-                       'eds_final_begin', 'eds_final_end', 'eds_final_patient_id', 'original_patient_id',
+                       'eds_final_begin', 'eds_final_end', 'eds_final_patient_id',
                        'eds_final_birth', 'eds_final_death', 'eds_final_birth_str',
-                       'date_from', 'date_to', 'patient_id_manual', 'stroke_onset_date', 'Referral', 'match_by']
+                       'date_from', 'date_to', 'patient_id_manual', 'stroke_onset_date', 'Referral', 'match_by',
+                       'multiple_id']
     scales_df.drop(columns_to_drop, axis=1, inplace=True)
     possible_value_ranges_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                                             'possible_ranges_for_variables.xlsx')
     possible_value_ranges = pd.read_excel(possible_value_ranges_file)
 
     glasgow_equivalents = ['Glasgow + pupilles', 'Glasgow + pupilles + sensibilité/motricité', 'Glasgow',
-                           'Glasgow  urgence',
-                           'Neurologie - Glasgow']
+                           'Glasgow  urgence', 'Neurologie - Glasgow']
     scales_df.loc[scales_df['scale'].isin(glasgow_equivalents), 'scale'] = 'Glasgow Coma Scale'
 
     NIHSS_equivalents = ['NIHSS - National Institute oh Health Stroke Scale',
                          'NIHSS - National Institute of Health Stroke Scale']
     scales_df.loc[scales_df['scale'].isin(NIHSS_equivalents), 'scale'] = 'NIHSS'
 
-    pain_scale_equivalents = ['Douleur - b - Echelle numérique', 'Douleur - a - EVA', 'Douleur - c - Echelle verbale']
+    pain_scale_equivalents = ['EVA', 'Echelle douleur numérique', 'Douleur - b - Echelle numérique',
+                              'Douleur - a - EVA', 'Douleur - c - Echelle verbale']
     scales_df.loc[scales_df['scale'].isin(pain_scale_equivalents), 'scale'] = 'pain scale'
     # drop rows with scale = 'Douleur - h - CPOT' as not comparable with other scales
     scales_df.drop(scales_df[scales_df['scale'].str.contains('CPOT')].index, inplace=True)
 
     # convert score to float
+    scales_df.dropna(subset=['score'], inplace=True)
+    remaining_non_numerical_values = \
+        scales_df[pd.to_numeric(scales_df['score'], errors='coerce').isnull()][
+            'score'].unique()
+    if len(remaining_non_numerical_values) > 0:
+        raise ValueError(f'Remaining non-numerical values: {remaining_non_numerical_values}')
     scales_df['score'] = pd.to_numeric(scales_df['score'], errors='coerce')
 
+    # restrict to possible ranges
     if verbose:
         print('Preprocessing NIHSS')
     cleaned_scales_df, _ = restrict_variable_to_possible_ranges(scales_df, 'NIHSS',
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -28,3 +28,32 @@ def create_registry_case_identification_column(df):
                                  + '_' + df['EDS_last_4_digits'].str.zfill(4).astype(str)
     return case_identification_column
 
+
+def correct_overwritten_patient_id(df:pd.DataFrame, eds_df:pd.DataFrame) -> pd.DataFrame:
+    """
+    Correct for overwritten patient_id
+    Overwritten patient_ids can be detected with / ehr_extraction_verification / detect_overwritten_patient_ids.py
+    Gist: in some extractions, the patient_id is overwritten with the eds_final_patient_id
+
+    Retrieve correct patient id by matching 'patient_id', 'eds_end_4digit', 'eds_manual', 'patient_id_manual'
+    on scale_df side with 'eds_final_patient_id', 'eds_end_4digit', 'eds_manual', 'patient_id_manual' on eds_df
+
+    Args:
+        df: dataframe with overwritten patient_id
+        eds_df: dataframe with correct patient_id used as reference
+    Returns:
+        df: dataframe with corrected patient_id
+    """
+
+    truncated_eds_df = eds_df[['patient_id', 'eds_end_4digit', 'eds_manual', 'patient_id_manual', 'eds_final_patient_id']]
+    truncated_eds_df.drop_duplicates(inplace=True)
+    df = pd.merge(df, truncated_eds_df,
+                         left_on=['patient_id', 'eds_end_4digit', 'eds_manual', 'patient_id_manual'],
+                         right_on=['eds_final_patient_id', 'eds_end_4digit', 'eds_manual', 'patient_id_manual'],
+                         suffixes=('', '_eds'), how='left')
+
+    df.drop(['patient_id', 'eds_final_patient_id_eds', 'nr'], axis=1, inplace=True)
+    df.rename(columns={'patient_id_eds': 'patient_id'}, inplace=True)
+
+    return df
+
