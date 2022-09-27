e3b920306e3f66ce31568e13981193650d634a30
Julian Klug <tensu.wave@gmail.com>
2022-09-27 21:26:44 +0200

    Add in hospital and 3month mortality as possible outcomes

--- prediction/mrs_outcome_prediction/data_loading/data_formatting.py
+++ prediction/mrs_outcome_prediction/data_loading/data_formatting.py
@@ -103,14 +103,22 @@ def link_patient_id_to_outcome(y: pd.DataFrame, outcome: str) -> pd.DataFrame:
     """
     all_pids = y[['patient_id', 'outcome']].copy()
 
-    accepted_outcomes = ['3M mRS 0-2', '3M mRS 0-1']
+    functional_outcomes = ['3M mRS 0-2', '3M mRS 0-1']
+    mortality_outcomes = ['Death in hospital', '3M Death']
+    accepted_outcomes = functional_outcomes + mortality_outcomes
     if outcome not in accepted_outcomes:
         raise ValueError('Outcome must be one of {}'.format(accepted_outcomes))
         raise ValueError('Reduction to single outcome is not implemented for {}'.format(outcome))
 
-    # replaces duplicated patient_ids with a single patient_id with minimum outcome
-    duplicated_pids = all_pids[all_pids.duplicated(subset='patient_id', keep=False)].copy()
-    reduced_pids = duplicated_pids.groupby('patient_id').min().reset_index()
+    if outcome in functional_outcomes:
+        # replaces duplicated patient_ids with a single patient_id with minimum outcome (worst functional outcome)
+        duplicated_pids = all_pids[all_pids.duplicated(subset='patient_id', keep=False)].copy()
+        reduced_pids = duplicated_pids.groupby('patient_id').min().reset_index()
+
+    if outcome in mortality_outcomes:
+        # replaces duplicated patient_ids with a single patient_id with maximum outcome (worst mortality outcome)
+        duplicated_pids = all_pids[all_pids.duplicated(subset='patient_id', keep=False)].copy()
+        reduced_pids = duplicated_pids.groupby('patient_id').max().reset_index()
 
     all_pids_no_duplicates = all_pids[~all_pids.duplicated(subset='patient_id', keep=False)].copy()
     all_pids_no_duplicates = all_pids_no_duplicates.append(reduced_pids)
