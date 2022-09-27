8f251e68cec4061b215ea8ae46ba7b7af7a4afe5
Julian Klug <tensu.wave@gmail.com>
2022-09-27 21:26:44 +0200

    Add in hospital and 3month mortality as possible outcomes

--- preprocessing/outcome_preprocessing/outcome_preprocessing.py
+++ preprocessing/outcome_preprocessing/outcome_preprocessing.py
@@ -60,6 +60,16 @@ def preprocess_outcomes(stroke_registry_data_path, patient_selection_path, verbo
     restricted_stroke_registry_df['3M delta mRS'] = restricted_stroke_registry_df['3M mRS'] - restricted_stroke_registry_df[
         'Prestroke disability (Rankin)']
 
+    # if death in hospital set 3M Death to yes
+    restricted_stroke_registry_df.loc[restricted_stroke_registry_df['Death in hospital'] == 'yes', '3M Death'] = 'yes'
+    # if 3M mRs == 6, set 3M Death to yes
+    restricted_stroke_registry_df.loc[restricted_stroke_registry_df['3M mRS'] == 6, '3M Death'] = 'yes'
+    # if 3M mRs not nan and not 6, set 3M Death to no
+    restricted_stroke_registry_df.loc[(restricted_stroke_registry_df['3M mRS'] != 6) &
+                                      (~restricted_stroke_registry_df['3M mRS'].isna())
+                                    &(restricted_stroke_registry_df['3M Death'].isna()), '3M Death'] = 'no'
+
+
     outcome_df = restricted_stroke_registry_df[["case_admission_id"] + outcome_columns]
 
     # restrict to plausible ranges
@@ -69,6 +79,13 @@ def preprocess_outcomes(stroke_registry_data_path, patient_selection_path, verbo
     # add binarised outcomes if 3M mRS is not nan
     outcome_df['3M mRS 0-1'] = np.where(outcome_df['3M mRS'].isna(), np.nan, np.where(outcome_df['3M mRS'] <= 1, 1, 0))
     outcome_df['3M mRS 0-2'] = np.where(outcome_df['3M mRS'].isna(), np.nan, np.where(outcome_df['3M mRS'] <= 2, 1, 0))
+
+    # binarise 3M Death and Death in hospital
+    outcome_df.loc[outcome_df['3M Death'] == 'yes', '3M Death'] = 1
+    outcome_df.loc[outcome_df['3M Death'] == 'no', '3M Death'] = 0
+    outcome_df.loc[outcome_df['Death in hospital'] == 'yes', 'Death in hospital'] = 1
+    outcome_df.loc[outcome_df['Death in hospital'] == 'no', 'Death in hospital'] = 0
+
 
     assert outcome_df['3M mRS 0-2'].value_counts().sum() == outcome_df['3M mRS'].value_counts().sum(), "Number of 3M mRS 0-2 not equal to 3M mRS"
     assert outcome_df['3M mRS 0-1'].value_counts().sum() == outcome_df['3M mRS'].value_counts().sum(), "Number of 3M mRS 0-1 not equal to 3M mRS"
