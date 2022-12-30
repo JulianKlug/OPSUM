9caf9a18c31dcfc1395ab9a09e3850f25db74912
Julian Klug <tensu.wave@gmail.com>
2022-12-30 15:20:18 +0100

    Use reference cat. encoding to encode external dataset

--- preprocessing/preprocessing_tools/encoding_categorical_variables/encode_categorical_variables.py
+++ preprocessing/preprocessing_tools/encoding_categorical_variables/encode_categorical_variables.py
@@ -3,27 +3,28 @@ import os
 
 categorical_variables = [
     'Sex',
- 'Referral',
- 'Prestroke disability (Rankin)',
- 'Antihypert. drugs pre-stroke',
- 'Lipid lowering drugs pre-stroke',
- 'Antiplatelet drugs',
- 'Anticoagulants',
- 'MedHist Hypertension',
- 'MedHist Diabetes',
- 'MedHist Hyperlipidemia',
- 'MedHist Smoking',
- 'MedHist Atrial Fibr.',
- 'MedHist CHD',
- 'MedHist PAD',
- 'MedHist cerebrovascular_event',
- 'categorical_onset_to_admission_time',
- 'wake_up_stroke',
- 'categorical_IVT',
- 'categorical_IAT'
+    'Referral',
+    'Prestroke disability (Rankin)',
+    'Antihypert. drugs pre-stroke',
+    'Lipid lowering drugs pre-stroke',
+    'Antiplatelet drugs',
+    'Anticoagulants',
+    'MedHist Hypertension',
+    'MedHist Diabetes',
+    'MedHist Hyperlipidemia',
+    'MedHist Smoking',
+    'MedHist Atrial Fibr.',
+    'MedHist CHD',
+    'MedHist PAD',
+    'MedHist cerebrovascular_event',
+    'categorical_onset_to_admission_time',
+    'wake_up_stroke',
+    'categorical_IVT',
+    'categorical_IAT'
 ]
 
-def encode_categorical_variables(df: pd.DataFrame, verbose:bool = True, log_dir:str = '') -> pd.DataFrame:
+def encode_categorical_variables(df: pd.DataFrame, reference_categorical_encoding_path: str = '',
+                                 verbose:bool = True, log_dir:str = '') -> pd.DataFrame:
     """
     Encode categorical variables as numeric values through one-hot encoding.
     The difference between binary and non-binary variables is irrelevant when one-hot encoding variables:
@@ -41,23 +42,50 @@ def encode_categorical_variables(df: pd.DataFrame, verbose:bool = True, log_dir:
             if variable not in categorical_variables:
                 print(f"'{variable}',")
 
+    if reference_categorical_encoding_path != '':
+        if verbose:
+            print(f'Loading reference categorical encoding from {reference_categorical_encoding_path}')
+        reference_categorical_encoding = pd.read_csv(reference_categorical_encoding_path)
+
     one_hot_encoded_df = df.copy()
     log_columns = ['sample_label', 'baseline_value', 'other_categories']
     log_df = pd.DataFrame(columns=log_columns)
 
     for categorical_variable in categorical_variables:
-        dummy_coded_temp = pd.get_dummies(one_hot_encoded_df[
-                                              one_hot_encoded_df.sample_label == categorical_variable],
-                                          columns=['value'], prefix=str(categorical_variable).lower(), drop_first=True)
+        if reference_categorical_encoding_path == '':
+            # If there is no reference categorical encoding, create one from the data
+            dummy_coded_temp = pd.get_dummies(one_hot_encoded_df[
+                                                  one_hot_encoded_df.sample_label == categorical_variable],
+                                              columns=['value'], prefix=str(categorical_variable).lower(), drop_first=True)
 
-        # find baseline values for each categorical variable
-        baseline_value = [var
-                          for var in one_hot_encoded_df[
-                              one_hot_encoded_df.sample_label == categorical_variable][
-                              'value'].unique()
-                          if str(var) not in
-                          [col_name.split(str(categorical_variable).lower() + '_')[-1] for col_name in
-                           dummy_coded_temp.columns]]
+            # find baseline values for each categorical variable
+            baseline_value = [var
+                              for var in one_hot_encoded_df[
+                                  one_hot_encoded_df.sample_label == categorical_variable][
+                                  'value'].unique()
+                              if str(var) not in
+                              [col_name.split(str(categorical_variable).lower() + '_')[-1] for col_name in
+                               dummy_coded_temp.columns]]
+        else:
+            # If there is a reference categorical encoding, use it
+            dummy_coded_temp = pd.get_dummies(one_hot_encoded_df[
+                                                  one_hot_encoded_df.sample_label == categorical_variable],
+                                              columns=['value'], prefix=str(categorical_variable).lower())
+            # remove baseline column
+            reference_baseline_value = reference_categorical_encoding[reference_categorical_encoding.sample_label == categorical_variable]['baseline_value'].values[0]\
+                .replace("[", "").replace("]", "").replace("'", "")
+            reference_baseline_column = str(categorical_variable).lower() + '_' + reference_baseline_value
+            dummy_coded_temp = dummy_coded_temp.drop(columns=[reference_baseline_column])
+            baseline_value = reference_baseline_value
+
+            # add columns for variables not present in the data but present in the reference encoding
+            reference_other_categories = reference_categorical_encoding[reference_categorical_encoding.sample_label == categorical_variable]['other_categories'].values[0]\
+                .replace("[", "").replace("]", "").replace("'", "").split(', ')
+            for category in reference_other_categories:
+                category_column = str(categorical_variable).lower() + '_' + category
+                if category_column not in dummy_coded_temp.columns:
+                    dummy_coded_temp[str(categorical_variable).lower() + '_' + category] = 0
+
 
         if log_dir != '':
             # other values
--- preprocessing/preprocessing_tools/preprocessing_verification/variable_presence_verification.py
+++ preprocessing/preprocessing_tools/preprocessing_verification/variable_presence_verification.py
@@ -22,20 +22,32 @@ def assert_selected_variables_presence(df: pd.DataFrame, selected_variables: lis
     return True
 
 
-def variable_presence_verification(normalised_df: pd.DataFrame, desired_time_range:int=72) -> bool:
+def variable_presence_verification(normalised_df: pd.DataFrame, target_feature_path: str = '',
+                                   desired_time_range:int=72) -> bool:
 
-    # Verifying presence of all features
+    # Verifying presence of all selected variables
     all_variables_present = []
+    all_features_present = []
     selected_variables_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'geneva_stroke_unit_preprocessing/variable_assembly/selected_variables.xlsx')
     selected_variables = pd.read_excel(selected_variables_path)['included']
+
+    if target_feature_path == '':
+        target_feature_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),
+                                           'prediction', 'mrs_outcome_prediction', 'LSTM', 'training',
+                                           'lstm_feature_order.xlsx')
+    target_features = pd.read_excel(target_feature_path, header=None)[0].tolist()
 
     for cid in tqdm(normalised_df.case_admission_id.unique()):
         temp_cid_df = normalised_df[(normalised_df.case_admission_id == cid)]
         for time_bin in range(desired_time_range):
+            # check if all selected variables are present
             all_variables_present.append(
                 assert_selected_variables_presence(temp_cid_df[temp_cid_df.relative_sample_date_hourly_cat == time_bin],
                                                    selected_variables))
+            # check if all target features are present (ie a variable can be encoded into multiple features)
+            all_features_present.append(set(temp_cid_df[temp_cid_df.relative_sample_date_hourly_cat == time_bin].sample_label.unique()) == set(target_features))
     assert all(all_variables_present), 'Not all selected variables are present in the final dataset'
+    assert all(all_features_present), f'Not all target features are present in the final dataset, as defined in {target_feature_path}'
 
     return True
 
@@ -43,12 +55,13 @@ def variable_presence_verification(normalised_df: pd.DataFrame, desired_time_ran
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--preprocessed_df_path', type=str, required=True)
+    parser.add_argument('-p', '--preprocessed_df_path', type=str, required=True)
     parser.add_argument('--desired_time_range', type=int, default=72)
+    parser.add_argument('--target_feature_path', type=str, default='')
     args = parser.parse_args()
 
     normalised_df = pd.read_csv(args.preprocessed_df_path)
-    variable_presence_verification(normalised_df, args.desired_time_range)
+    variable_presence_verification(normalised_df, args.target_feature_path, args.desired_time_range)
 
 
 
