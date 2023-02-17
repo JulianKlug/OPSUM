e974d23d95b748f963a391a2909d802cc8b82d0b
Guillaume Leclerc <leclerc@mit.edu>
2023-02-17 17:37:03 -0500

    Add OPSUMTransformer architecture

---
+++ prediction/outcome_prediction/Transformer/architecture.py
@@ -0,0 +1,160 @@
+import torch as ch
+import numpy as np
+import copy
+from torch import nn
+import torch
+import math
+from torch.autograd import Variable
+from copy import deepcopy
+from torch.nn import LayerNorm
+import torch.nn.functional as F
+
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+    def __init__(self, d_model, dropout, max_len=5000, factor=0.1):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0) * factor
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)], 
+                         requires_grad=False)
+        return self.dropout(x)
+    
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+        
+    def forward(self, x):
+        "Pass the input (and mask) through each layer in turn."
+        for layer in self.layers:
+            x = layer(x)
+        return self.norm(x)
+    
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+def attention(query, key, value, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) \
+             / math.sqrt(d_k)
+    scores = scores.tril()
+    p_attn = F.softmax(scores, dim = -1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+class EncoderLayer(nn.Module):
+    "Encoder is made up of self-attn and feed forward (defined below)"
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x):
+        "Follow Figure 1 (left) for connections."
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x))
+        return self.sublayer[1](x, self.feed_forward)
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        "Take in model size and number of heads."
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        # We assume d_v always equals d_k
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+        
+    def forward(self, query, key, value):
+        "Implements Figure 2"
+        nbatches = query.size(0)
+        
+        # 1) Do all the linear projections in batch from d_model => h x d_k 
+        query, key, value = \
+            [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+             for l, x in zip(self.linears, (query, key, value))]
+        
+        # 2) Apply attention on all the projected vectors in batch. 
+        x, self.attn = attention(query, key, value, dropout=self.dropout)
+        
+        # 3) "Concat" using a view and apply a final linear. 
+        x = x.transpose(1, 2).contiguous() \
+             .view(nbatches, -1, self.h * self.d_k)
+        return self.linears[-1](x)                   
+                   
+class PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation."
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+    
+class FinalClassification(nn.Module):
+    
+    def __init__(self, model_dim, num_classes=1):
+        super().__init__()
+        self.projector = nn.Linear(model_dim, num_classes)
+        self.num_classes = num_classes
+        
+    def forward(self, x):
+        bs = x.shape[0]
+        return self.projector(x.reshape(-1, x.shape[2])).reshape(bs, -1, self.num_classes)
+    
+class OPSUMTransformer(nn.Module):
+    
+    def __init__(self, input_dim, num_layers, model_dim, ff_dim,
+                 num_heads, dropout, num_classes=1, pos_encode_factor=0.1,
+                 max_dim=5000):
+        super().__init__()
+        self.embedder = nn.Linear(input_dim, model_dim)
+        self.pe = PositionalEncoding(model_dim, dropout, max_dim, factor=pos_encode_factor)
+        ff = PositionwiseFeedForward(model_dim, ff_dim, dropout)
+        attn = MultiHeadedAttention(attention_heads, model_dim)
+        c = copy.deepcopy
+        self.encoder = Encoder(EncoderLayer(model_dim, c(attn), c(ff), dropout), num_layers)
+        self.classifier = FinalClassification(model_dim, 1)
+        
+    def forward(self, x):
+        x = self.embedder(x)
+        x = self.pe(x)
+        x = self.encoder(x)
+        x = self.classifier(x)
+        return x
