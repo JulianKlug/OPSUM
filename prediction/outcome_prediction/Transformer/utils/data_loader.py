18ffd3bcbfd3bbf75e56c7e0f8329dc7238193ee
Julian Klug <tensu.wave@gmail.com>
2023-04-04 07:36:10 +0200

    Refactored for testing

---
+++ prediction/outcome_prediction/Transformer/utils/data_loader.py
@@ -0,0 +1,75 @@
+import os
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import pandas as pd
+import numpy as np
+from prediction.outcome_prediction.data_loading.data_formatting import format_to_2d_table_with_time, \
+    link_patient_id_to_outcome, features_to_numpy, numpy_to_lookup_table
+from prediction.utils.utils import check_data
+
+
+def load_data(features_path, labels_path, outcome, test_size, n_splits, seed):
+    ### LOAD THE DATA
+    X, y = format_to_2d_table_with_time(feature_df_path=features_path, outcome_df_path=labels_path,
+                                        outcome=outcome)
+
+    # test if data is corrupted
+    check_data(X)
+
+    """
+    SPLITTING DATA
+    Splitting is done by patient id (and not admission id) as in case of the rare multiple admissions per patient there
+    would be a risk of data leakage otherwise split 'pid' in TRAIN and TEST pid = unique patient_id
+    """
+    # Reduce every patient to a single outcome (to avoid duplicates)
+    all_pids_with_outcome = link_patient_id_to_outcome(y, outcome)
+    pid_train, pid_test, y_pid_train, y_pid_test = train_test_split(all_pids_with_outcome.patient_id.tolist(),
+                                                                    all_pids_with_outcome.outcome.tolist(),
+                                                                    stratify=all_pids_with_outcome.outcome.tolist(),
+                                                                    test_size=test_size,
+                                                                    random_state=seed)
+    # Preprocess overall train data
+    train_X_df = X[X.patient_id.isin(pid_train)]
+    train_y_df = y[y.patient_id.isin(pid_train)]
+    train_X_np = features_to_numpy(train_X_df,
+                                   ['case_admission_id', 'relative_sample_date_hourly_cat', 'sample_label', 'value'])
+    train_y_np = np.array([train_y_df[train_y_df.case_admission_id == cid].outcome.values[0] for cid in
+                           train_X_np[:, 0, 0, 0]]).astype('float32')
+    train_X_np = train_X_np[:, :, :, -1].astype('float32')
+
+
+    # Preprocess overall test data
+    test_X_df = X[X.patient_id.isin(pid_test)]
+    test_y_df = y[y.patient_id.isin(pid_test)]
+    test_X_np = features_to_numpy(test_X_df,
+                                  ['case_admission_id', 'relative_sample_date_hourly_cat', 'sample_label', 'value'])
+    test_y_np = np.array([test_y_df[test_y_df.case_admission_id == cid].outcome.values[0] for cid in
+                          test_X_np[:, 0, 0, 0]]).astype('float32')
+    # create look-up table for case_admission_ids, sample_labels and relative_sample_date_hourly_cat
+    test_features_lookup_table = numpy_to_lookup_table(test_X_np)
+    # Remove the case_admission_id, sample_label, and time_step_label columns from the data
+    test_X_np = test_X_np[:, :, :, -1].astype('float32')
+
+    # Preprocess k-fold train/validation data
+    kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
+    splits = []
+    for fold_pid_train_idx, fold_pid_val_idx in kfold.split(pid_train, y_pid_train):
+        fold_train_pidx = np.array(pid_train)[fold_pid_train_idx]
+        fold_val_pidx = np.array(pid_train)[fold_pid_val_idx]
+
+        fold_X_train_df = X.loc[X.patient_id.isin(fold_train_pidx)]
+        fold_y_train_df = y.loc[y.patient_id.isin(fold_train_pidx)]
+        fold_X_val_df = X.loc[X.patient_id.isin(fold_val_pidx)]
+        fold_y_val_df = y.loc[y.patient_id.isin(fold_val_pidx)]
+
+        fold_X_train = features_to_numpy(fold_X_train_df, ['case_admission_id', 'relative_sample_date_hourly_cat', 'sample_label', 'value'])
+        fold_X_val = features_to_numpy(fold_X_val_df, ['case_admission_id', 'relative_sample_date_hourly_cat', 'sample_label', 'value'])
+
+        fold_y_train = np.array([fold_y_train_df[fold_y_train_df.case_admission_id == cid].outcome.values[0] for cid in fold_X_train[:, 0, 0, 0]]).astype('float32')
+        fold_y_val = np.array([fold_y_val_df[fold_y_val_df.case_admission_id == cid].outcome.values[0] for cid in fold_X_val[:, 0, 0, 0]]).astype('float32')
+
+        fold_X_train = fold_X_train[:, :, :, -1].astype('float32')
+        fold_X_val = fold_X_val[:, :, :, -1].astype('float32')
+
+        splits.append((fold_X_train, fold_X_val, fold_y_train, fold_y_val))
+
+    return (pid_train, pid_test), (train_X_np, train_y_np), (test_X_np, test_y_np), splits, test_features_lookup_table
---
+++ prediction/outcome_prediction/Transformer/utils/utils.py
@@ -0,0 +1,77 @@
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import torch as ch
+from torch.utils.data import TensorDataset
+
+try:
+    from pytorch_lightning.loggers import LightningLoggerBase
+except:
+    from pytorch_lightning.loggers.logger import Logger as LightningLoggerBase
+
+
+class DictLogger(LightningLoggerBase):
+    """PyTorch Lightning `dict` logger."""
+
+    def __init__(self, version):
+        super(DictLogger, self).__init__()
+        self.metrics = []
+        self._version = version
+
+    def log_metrics(self, metrics, step=None):
+        self.metrics.append(metrics)
+
+    @property
+    def version(self):
+        return self._version
+
+    @property
+    def experiment(self):
+        """Return the experiment object associated with this logger."""
+
+    def log_hyperparams(self, params):
+        """
+        Record hyperparameters.
+        Args:
+            params: :class:`~argparse.Namespace` containing the hyperparameters
+        """
+
+    @property
+    def name(self):
+        """Return the experiment name."""
+        return 'optuna'
+
+
+def prepare_dataset(scenario, balanced=False, aggregate=False, rescale=True, use_gpu=True):
+    X_train, X_val, y_train, y_val = scenario
+    scaler = StandardScaler()
+
+    if rescale:
+        X_train = scaler.fit_transform(X_train.reshape(-1, 84)).reshape(X_train.shape)
+        X_val = scaler.transform(X_val.reshape(-1, 84)).reshape(X_val.shape)
+
+    if balanced:
+        X_train_neg = X_train[y_train == 0]
+        X_train_pos = X_train[np.random.choice(np.where(y_train==1)[0], X_train_neg.shape[0])]
+        X_train = np.concatenate([X_train_neg, X_train_pos])
+        y_train = np.concatenate([np.zeros(X_train_neg.shape[0]), np.ones(X_train_pos.shape[0])])
+
+    if aggregate:
+        X_train = feature_aggregation(X_train)
+        X_val = feature_aggregation(X_val)
+
+    if use_gpu:
+        train_dataset = TensorDataset(ch.from_numpy(X_train).cuda(), ch.from_numpy(y_train.astype(np.int32)).cuda())
+        val_dataset = TensorDataset(ch.from_numpy(X_val).cuda(), ch.from_numpy(y_val.astype(np.int32)).cuda())
+    else:
+        train_dataset = TensorDataset(ch.from_numpy(X_train), ch.from_numpy(y_train.astype(np.int32)))
+        val_dataset = TensorDataset(ch.from_numpy(X_val), ch.from_numpy(y_val.astype(np.int32)))
+    return train_dataset, val_dataset
+
+
+def feature_aggregation(x):
+    # given an array of shape (batch_size, seq_len, feature_dim), for each feature_dim, compute the cumulative average / max / min and add them to the feature_dim
+    time_avg = np.cumsum(x, axis=1) / np.arange(1, x.shape[1] + 1)[:, np.newaxis]
+    time_max = np.maximum.accumulate(x, axis=1)
+    time_min = np.minimum.accumulate(x, axis=1)
+    return np.concatenate([x, time_avg, time_max, time_min], axis=-1).astype(np.float32)
+
