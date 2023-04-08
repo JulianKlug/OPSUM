309067633ea0d969e3e4f69ec9170e721bdc8b5d
Julian Klug <tensu.wave@gmail.com>
2023-04-08 16:30:57 +0200

    Added statistical ROC comparison

--- prediction/outcome_prediction/treeModel/test_xgb.py
+++ prediction/outcome_prediction/treeModel/test_xgb.py
@@ -10,9 +10,9 @@ from sklearn.metrics import roc_auc_score, matthews_corrcoef, accuracy_score, pr
 from prediction.outcome_prediction.treeModel.feature_aggregration_xgboost import evaluate_model
 
 
-def test_model(max_depth:int, learning_rate:float, n_estimators:int, reg_lambda:int, alpha:int,
+def test_model(max_depth:int, learning_rate:float, n_estimators:int, reg_lambda:int, alpha:int, moving_average:bool,
                  outcome:str, features_df_path:str, outcomes_df_path:str, output_dir:str):
-    optimal_model_df, trained_models, test_dataset = evaluate_model(max_depth, learning_rate, n_estimators, reg_lambda, alpha,
+    optimal_model_df, trained_models, test_dataset = evaluate_model(max_depth, learning_rate, n_estimators, reg_lambda, alpha, moving_average,
                                                         outcome, features_df_path, outcomes_df_path, output_dir, save_models=True)
 
     X_test, y_test = test_dataset
@@ -139,7 +139,8 @@ if __name__ == '__main__':
     best_parameters_df = pd.read_csv(cli_args.best_parameters_path)
 
     result_df, bootstrapping_data, testing_data = test_model(int(best_parameters_df['max_depth'][0]), best_parameters_df['learning_rate'][0], int(best_parameters_df['n_estimators'][0]), best_parameters_df['reg_lambda'][0], best_parameters_df['alpha'][0],
-                cli_args.outcome, cli_args.feature_df_path, cli_args.outcome_df_path, cli_args.output_dir)
+                                                             bool(best_parameters_df['moving_average'][0]),
+                                                                cli_args.outcome, cli_args.feature_df_path, cli_args.outcome_df_path, cli_args.output_dir)
 
     result_df.to_csv(os.path.join(cli_args.output_dir, 'test_XGB_results.csv'), sep=',', index=False)
 
