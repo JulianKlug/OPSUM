d283a76a1f4fb90d9eea32f42db7bb83969b1a23
Julian Klug <tensu.wave@gmail.com>
2023-04-16 20:03:06 +0200

    Calibration for Transformer model

--- prediction/utils/visualisation_helper_functions.py
+++ prediction/utils/visualisation_helper_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 from colormath.color_objects import sRGBColor
 from colormath.color_conversions import convert_color
+from matplotlib import pyplot as plt
+from sklearn.calibration import calibration_curve
 
 
 def reverse_normalisation_for_subj(norm_subj_df, normalisation_parameters_df):
@@ -49,3 +51,24 @@ def create_palette(start_rgb, end_rgb, n, colorspace, extrapolation_length):
 
 def hex_to_rgb_color(hex):
     return sRGBColor(*[int(hex[i + 1:i + 3], 16) for i in (0, 2 ,4)], is_upscaled=True)
+
+
+def plot_calibration_curve(y_true, y_prob, n_bins=5, ax=None, hist=True, normalize=False, title=None):
+    prob_true, prob_pred = calibration_curve(y_true, y_prob, n_bins=n_bins, normalize=normalize)
+    if ax is None:
+        ax = plt.gca()
+    if hist:
+        ax.hist(y_prob, weights=np.ones_like(y_prob) / len(y_prob), alpha=.4,
+               bins=np.maximum(10, n_bins))
+    ax.plot([0, 1], [0, 1], ':', c='k')
+    curve = ax.plot(prob_pred, prob_true, marker="o")
+
+    ax.set_xlabel("predicted probability")
+    ax.set_ylabel("fraction of positive samples")
+
+    ax.set(aspect='equal')
+
+    if title is not None:
+        ax.set_title(title)
+
+    return curve
