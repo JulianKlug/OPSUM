0d149df86fff508be7753fdf262bdfeae1913a80
Julian Klug <tensu.wave@gmail.com>
2025-01-06 15:43:38 +0100

    adapted callbacks for all metrics

--- prediction/outcome_prediction/Transformer/utils/callbacks.py
+++ prediction/outcome_prediction/Transformer/utils/callbacks.py
@@ -6,22 +6,38 @@ class MyEarlyStopping(Callback):
     best_so_far = 0
     last_improvement = 0
 
-    def __init__(self, step_limit=10, metric='val_auroc'):
+    def __init__(self, step_limit=10, metric='val_auroc', direction='max'):
         super().__init__()
         self.step_limit = step_limit
         self.metric = metric
+        self.direction = direction
 
     def on_validation_end(self, trainer, pl_module):
         logs = trainer.callback_metrics
         val_metric = logs[self.metric].item()
 
-        if val_metric > self.best_so_far:
-            self.last_improvement = 0
-        else:
-            self.last_improvement += 1
+        if self.direction == 'max':
+            if val_metric > self.best_so_far:
+                self.last_improvement = 0
+            else:
+                self.last_improvement += 1
 
-        print(self.last_improvement)
-        trainer.should_stop = val_metric < 0.75 * self.best_so_far or self.last_improvement > self.step_limit or \
-                    (trainer.current_epoch > 10 and val_metric < 0.55)
+            print(self.last_improvement)
+
+            if self.metric == 'val_auroc':
+                trainer.should_stop = val_metric < 0.75 * self.best_so_far or self.last_improvement > self.step_limit or \
+                            (trainer.current_epoch > 10 and val_metric < 0.55)
+            else:
+                trainer.should_stop = self.last_improvement > self.step_limit
+
+        if self.direction == 'min':
+            if val_metric < self.best_so_far:
+                self.last_improvement = 0
+            else:
+                self.last_improvement += 1
+
+            trainer.should_stop = self.last_improvement > self.step_limit
+
+
 
         self.best_so_far = max(val_metric, self.best_so_far)
