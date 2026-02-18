4067d14f230603bb0eac0c01bde61508c756434e
Julian Klug <tensu.wave@gmail.com>
2026-02-18 16:22:09 +0100

    updated MIMIC preprocessing to be compatible with short term outcomes

--- preprocessing/mimic_preprocessing/preprocessing_pipeline/preprocessing_pipeline.py
+++ preprocessing/mimic_preprocessing/preprocessing_pipeline/preprocessing_pipeline.py
@@ -8,7 +8,9 @@ import pandas as pd
 
 from preprocessing.mimic_preprocessing.database_assembly.database_assembly import assemble_variable_database
 from preprocessing.mimic_preprocessing.database_assembly.relative_timestamps import transform_to_relative_timestamps
-from preprocessing.mimic_preprocessing.outcome_preprocessing.outcome_preprocessing import preprocess_outcomes
+from preprocessing.mimic_preprocessing.outcome_preprocessing.long_term_outcomes.outcome_preprocessing import preprocess_outcomes
+from preprocessing.mimic_preprocessing.outcome_preprocessing.short_term_outcomes.short_term_outcomes_preprocessing import \
+    preprocess_short_term_outcomes
 from prediction.utils.utils import ensure_dir
 from preprocessing.preprocessing_tools.resample_to_time_bins.resample_to_hourly_features import resample_to_hourly_features
 from preprocessing.preprocessing_tools.encoding_categorical_variables.encode_categorical_variables import encode_categorical_variables
@@ -18,13 +20,20 @@ from preprocessing.preprocessing_tools.preprocessing_verification.outcome_presen
 from preprocessing.preprocessing_tools.preprocessing_verification.variable_presence_verification import variable_presence_verification
 
 
-def preprocess(extracted_tables_path: str, admission_notes_data_path: str,
-                reference_population_imputation_path: str,
-                reference_population_normalisation_parameters_path: str,
-                reference_categorical_encoding_path: str,
-                preproccessed_monitoring_data_path: str = '',
-                mimic_admission_nihss_db_path: str = '',
-                log_dir: str = '', verbose:bool=True, desired_time_range:int=72) -> pd.DataFrame:
+def preprocess(
+    extracted_tables_path: str,
+    admission_notes_data_path: str,
+    reference_population_imputation_path: str,
+    reference_population_normalisation_parameters_path: str,
+    reference_categorical_encoding_path: str,
+    preproccessed_monitoring_data_path: str = '',
+    mimic_admission_nihss_db_path: str = '',
+    log_dir: str = '',
+    include_short_term_outcomes: bool = True,
+    short_term_outcomes_config: dict = {},
+    verbose: bool = True,
+    desired_time_range: int = 72,
+) -> pd.DataFrame:
     """
     Apply geneva_stroke_unit_preprocessing pipeline detailed in ./geneva_stroke_unit_preprocessing/readme.md to the MIMIC-III dataset.
 
@@ -90,22 +99,35 @@ def preprocess(extracted_tables_path: str, admission_notes_data_path: str,
 
     # 11. geneva_stroke_unit_preprocessing outcomes
     outcome_table_path = os.path.join(extracted_tables_path, 'outcome_df.csv')
-    preprocessed_outcomes_df = preprocess_outcomes(outcome_table_path, verbose=verbose)
+    preprocessed_long_term_outcomes_df = preprocess_outcomes(outcome_table_path, verbose=verbose)
+    if include_short_term_outcomes:
+        preprocessed_short_term_outcomes_df = preprocess_short_term_outcomes(
+            restricted_feature_df,
+            **short_term_outcomes_config,
+        )
+        preprocessed_outcomes = (preprocessed_long_term_outcomes_df, preprocessed_short_term_outcomes_df)
+    else:
+        preprocessed_outcomes = (preprocessed_long_term_outcomes_df,)
 
-    return normalised_df, preprocessed_outcomes_df
+    return normalised_df, preprocessed_outcomes
 
 
 
 def preprocess_and_save(
-                extracted_tables_path: str, admission_notes_data_path: str,
-                reference_population_imputation_path: str,
-                reference_population_normalisation_parameters_path: str,
-                reference_categorical_encoding_path: str,
-                output_dir: str,
-                preproccessed_monitoring_data_path: str = '',
-                mimic_admission_nihss_db_path: str = '',
-                feature_file_prefix:str = 'preprocessed_features', outcome_file_prefix:str = 'preprocessed_outcomes',
-                verbose:bool=True):
+    extracted_tables_path: str,
+    admission_notes_data_path: str,
+    reference_population_imputation_path: str,
+    reference_population_normalisation_parameters_path: str,
+    reference_categorical_encoding_path: str,
+    output_dir: str,
+    preproccessed_monitoring_data_path: str = '',
+    mimic_admission_nihss_db_path: str = '',
+    include_short_term_outcomes: bool = True,
+    short_term_outcomes_config: dict = {},
+    feature_file_prefix: str = 'preprocessed_features',
+    outcome_file_prefix: str = 'preprocessed_outcomes',
+    verbose: bool = True,
+):
 
     timestamp = time.strftime("%d%m%Y_%H%M%S")
     desired_time_range = 72
@@ -118,26 +140,49 @@ def preprocess_and_save(
     with open(os.path.join(log_dir, 'preprocessing_arguments.json'), 'w') as fp:
         json.dump(saved_args, fp, indent=4)
 
-    preprocessed_feature_df, preprocessed_outcome_df = preprocess(extracted_tables_path, admission_notes_data_path,
-                           reference_population_imputation_path, reference_population_normalisation_parameters_path,
-                           reference_categorical_encoding_path,
-                           preproccessed_monitoring_data_path, mimic_admission_nihss_db_path,
-                                                                  log_dir=log_dir, verbose=verbose,
-                                                                  desired_time_range=desired_time_range)
+    preprocessed_feature_df, preprocessed_outcomes = preprocess(
+        extracted_tables_path,
+        admission_notes_data_path,
+        reference_population_imputation_path,
+        reference_population_normalisation_parameters_path,
+        reference_categorical_encoding_path,
+        preproccessed_monitoring_data_path,
+        mimic_admission_nihss_db_path,
+        log_dir=log_dir,
+        include_short_term_outcomes=include_short_term_outcomes,
+        short_term_outcomes_config=short_term_outcomes_config,
+        verbose=verbose,
+        desired_time_range=desired_time_range,
+    )
 
     features_save_path = os.path.join(output_dir, f'{feature_file_prefix}_{timestamp}.csv')
     outcomes_save_path = os.path.join(output_dir, f'{outcome_file_prefix}_{timestamp}.csv')
 
     preprocessed_feature_df.to_csv(features_save_path, index=False)
-    preprocessed_outcome_df.to_csv(outcomes_save_path, index=False)
+    if include_short_term_outcomes:
+        preprocessed_long_term_outcome_df, preprocessed_short_term_outcome_df = preprocessed_outcomes
+        short_term_outcomes_path = os.path.join(
+            output_dir,
+            f'{outcome_file_prefix}_short_term_{timestamp}.csv',
+        )
+        preprocessed_short_term_outcome_df.to_csv(short_term_outcomes_path, index=False)
+    else:
+        preprocessed_long_term_outcome_df = preprocessed_outcomes[0]
 
-    if preproccessed_monitoring_data_path != '':
+    preprocessed_long_term_outcome_df.to_csv(outcomes_save_path, index=False)
+
+    if preproccessed_monitoring_data_path != '' and not preproccessed_monitoring_data_path is None:
         # copy file to log dir
         shutil.copy(preproccessed_monitoring_data_path, log_dir)
 
     # verification of geneva_stroke_unit_preprocessing
     variable_presence_verification(preprocessed_feature_df, desired_time_range=desired_time_range)
-    outcome_presence_verification(preprocessed_outcome_df, preprocessed_feature_df, log_dir=log_dir, outcomes=['Death in hospital', '3M Death'])
+    outcome_presence_verification(
+        preprocessed_long_term_outcome_df,
+        preprocessed_feature_df,
+        log_dir=log_dir,
+        outcomes=['Death in hospital', '3M Death'],
+    )
 
 
 if __name__ == '__main__':
@@ -159,10 +204,21 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output_dir', type=str, help='Output directory')
     parser.add_argument('-m', '--preprocessed_monitoring_path', type=str, help='Path to preprocessed monitoring data')
     parser.add_argument('-mi', '--mimic_admission_nihss_db_path', type=str, help='Path to the mimic nihss data')
+    parser.add_argument('-s', '--include_short_term_outcomes', action='store_true', help='Include short term outcomes', default=False)
+    parser.add_argument('-end_min_repeats', '--end_min_repeats', type=bool, help='Whether to require a minimum number of repeated measurements for detection of END', default=False)
+    parser.add_argument('-end_min_delta', '--end_min_delta', type=int, help='The minimum difference in NIHSS scores to consider as END', default=4)
+    parser.add_argument('-end_keep_multiple_events', '--end_keep_multiple_events', type=bool, help='If True, allows detection of multiple deterioration events by resetting the baseline after each event', default=True)
     args = parser.parse_args()
 
     preprocess_and_save(args.ehr_tables_path, args.admission_notes_path,
                         args.reference_population_imputation_path, args.reference_population_normalisation_parameters_path,
                         args.reference_categorical_encoding_path,
                         args.output_dir,
-                        args.preprocessed_monitoring_path, args.mimic_admission_nihss_db_path, verbose=True)
+                        args.preprocessed_monitoring_path, args.mimic_admission_nihss_db_path,
+                        include_short_term_outcomes=args.include_short_term_outcomes,
+                        short_term_outcomes_config={
+                            'end_require_min_repeats': args.end_min_repeats,
+                            'end_min_delta': args.end_min_delta,
+                            'end_keep_multiple_events': args.end_keep_multiple_events,
+                        },
+                        verbose=True)
